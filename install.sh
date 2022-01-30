1e8ab70eeb119d0941de7393814444a8494cf75a
Yago Yañez Marco <yago.yanez@gmail.com>
2022-01-30 08:55:36 +0100

    Probado script de instalación en el ordenador del comedor. Arreglados un par de fallos

--- install.sh
+++ install.sh
@@ -3,13 +3,14 @@
 # Check if we are in SUDO mode
 
 if [ ! -z "$SUDO_USER" ]; then
-    apt-get install gir1.2-appindicator3
-    apt-get install python3-pip
+    apt-get install -y gir1.2-appindicator3
+    apt-get install -y python3-pip
     pip install pyserial==3.3
     usermod -a -G dialout $(logname)
     mkdir /opt/serial_to_app_indicator/
     cp serial_to_app_indicator.py /opt/serial_to_app_indicator/serial_to_app_indicator.py
     chmod 777 /opt/serial_to_app_indicator/serial_to_app_indicator.py
+    cp image.png /opt/serial_to_app_indicator/image.png
     echo "================================================================"
     echo "================================================================"
     echo "You must restart the session to get USB - serial port privileges"
